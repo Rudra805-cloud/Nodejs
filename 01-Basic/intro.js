d1e7863a609ab7f51b324a57144a2ed0b115aedc
Rudra805 <rudramishra9949@gmail.com>
2026-01-19 22:29:56 +0530

    intro to nodejs

---
+++ 01-Basic/intro.js
@@ -0,0 +1,8 @@
+//Q->what is node js
+/*ans=>node js is run time envirement for js its same like for run java we required a javac package same for run js outside of chrome we required node js*/
+
+/*In our chrome or any search engine we have  in chorome we hava engine likee a V8 engine,in safari engine of apple ,and js run on that */
+
+//NODEJS=>(V8 engine ke bahut saare ui related feture dom etc ko nikala aur only un chizo ko rakha hai vo server site pe kaam aate hai ) +(C++)
+
+//NPM =>node package mannager =>do task of install and delete and manage packages 
---
+++ 02-Helloworld/helloworld.js
@@ -0,0 +1,7 @@
+console.log("Hellow there ")
+
+console.log(window) //error::window is not defined(why?->because V8 engine me un feture like alert dom etc ko hata kar only un chizo ko rakha jo server side pe required hai ) 
+
+//npm init->node package manager initalize
+//in package json 
+//we write multiple script which help us in run file 
